23a3936954ecfe5623800f7f18bcccb5ae0436bb
Dominik <domi9132@gmail.com>
2026-01-06 18:52:47 +0100

    Remove project-level templates directory path

--- montra/settings.py
+++ montra/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'montra.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'montra' / 'templates'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
